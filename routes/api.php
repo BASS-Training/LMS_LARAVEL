e3a24bad817b4f64a13764f8c73389d24cbac568
BASS-Training <basselearning.com>
2026-05-20 09:57:15 +0700

    reset quiz integration

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,5 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CourseApiController;
-use App\Http\Controllers\Api\QuizApiController;
 
 Route::get('/mobile/courses', [CourseApiController::class, 'index']);
-
-// Quiz API for mobile
-Route::get('/quizzes/by-lesson/{lessonId}', [QuizApiController::class, 'getByLesson']);
-Route::post('/quizzes/{quiz}/attempts', [QuizApiController::class, 'startAttempt']);
-Route::post('/quizzes/{quiz}/attempts/{attempt}/submit', [QuizApiController::class, 'submitAttempt']);
-
-// Legacy aliases for older clients
-Route::get('/mobile/quizzes/by-lesson/{lessonId}', [QuizApiController::class, 'getByLesson']);
-Route::post('/mobile/quizzes/{quiz}/attempts', [QuizApiController::class, 'startAttempt']);
-Route::post('/mobile/quizzes/{quiz}/attempts/{attempt}/submit', [QuizApiController::class, 'submitAttempt']);
